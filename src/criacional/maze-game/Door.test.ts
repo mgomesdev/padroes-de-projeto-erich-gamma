f99d80216c6498c72b944ea438fc3335bbe08b48
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2025-03-30 16:00:18 -0300

    test: cria teste inicial de door

--- src/criacional/maze-game/Door.test.ts
+++ src/criacional/maze-game/Door.test.ts
@@ -1,5 +1,13 @@
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
+import Door from "./Door";
+import Room from "./Room";
 
 describe("Classe Door (porta)", () => {
-   it("Deve definir o componente Door para ser utilizado no labirinto");
+   it("Deve definir o componente Door para ser utilizado no labirinto", () => {
+      const room1 = new Room(1);
+      const room2 = new Room(2);
+      const door = new Door(room1, room2);
+
+      expect(door.enter()).toBe(0);
+   });
 });
