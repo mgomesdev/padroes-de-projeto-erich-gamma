2df84d1ff06a11de6c33cd9b48167f4371d61a97
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2025-04-20 18:17:01 -0300

    refactor: cria a classe MazeFactory

--- src/criacional/abstract-factory/MazeFactory.test.ts
+++ src/criacional/abstract-factory/MazeFactory.test.ts
@@ -1,7 +1,22 @@
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
+import MazeFactory from "./MazeFactory";
+import Room from "../maze-game/Room";
+import Wall from "../maze-game/Wall";
+import Door from "../maze-game/Door";
 
 describe("A classe MazeFactory pode criar componentes de labirintos", () => {
-   it.todo("Ela constroi salas, paredes e portas entre salas");
+   it("Ela constroi salas, paredes e portas entre salas", () => {
+      const mazeFactory = new MazeFactory();
+      const room1 = mazeFactory.MakeRoom(3);
+      const room2 = mazeFactory.MakeRoom(7);
+      const wall = mazeFactory.MakeWall();
+      const door = mazeFactory.MakeDoor(room1, room2);
+
+      expect(room1).toBeInstanceOf(Room);
+      expect(room2).toBeInstanceOf(Room);
+      expect(wall).toBeInstanceOf(Wall);
+      expect(door).toBeInstanceOf(Door);
+   });
    it.todo(
       "Pode ser usada por um programa que lê plantas de labirintos de uma rquivo e constroi o correspondente labirinto"
    );
